e83496568a4411c949b63bf91788bdd612cc53c8
Ash Kumar <ashkumar12@yahoo.com>
2018-05-20 16:54:54 -0400

    setting up dev env stuff

---
+++ main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CryptoLockApp from './CryptoLockApp.jsx';
+
+ReactDOM.render(<CryptoLockApp />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var config = {
+   entry: './main.js',
+   output: {
+      path:'/',
+      filename: 'index.js',
+   },
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+   module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            cacheDirectory: true,
+            presets: ['react', 'es2015']
+          }
+        }
+      ]
+   }
+}
+module.exports = config;
