f1091dad4e63ba87f31a4f40d7d395b1b2cd13ab
Ash Kumar <ashkumar12@yahoo.com>
2018-05-20 20:18:07 -0400

    set up form structure

--- CryptoLockApp.jsx
+++ CryptoLockApp.jsx
@@ -1,12 +1,62 @@
 import React from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
 
 class CryptoLockApp extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World!!!
-         </div>
-      );
+	constructor(props) {
+    super(props);
+  	this.state = {
+      date: moment(),
+      amount: 0
+    };
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleAmountChange = this.handleAmountChange.bind(this);
+  };
+
+  handleAmountChange(input) {
+    this.setState({amount:input.target.value});
+  };
+
+  handleDateChange(input) {
+    this.setState({date:input});
+  };
+
+  render() {
+		return (
+      		<div>
+	      	 <h1>CryptoLock</h1>
+	      		<form>
+	      	  	<p>{this.state.date.toString()}</p>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.handleDateChange}
+              />
+
+              <div className="grid-container">
+               <div className="grid-x grid-margin-x">
+                <div className="cell small-4">
+                  
+                </div>
+                <div className="cell small-4 small-centered text-center">
+                  <label>
+                    <span className="display-inline-block margin-right-10">Amount:</span>
+                    <span className="display-inline-block"><input className="text-center" type="text" value={this.state.amount} onChange={this.handleAmountChange} /></span>
+                  </label>
+                </div>
+                <div className="cell small-4">
+                  
+                </div>
+               </div>
+              </div>
+  
+              <div className="button-group align-center">
+                <button type="button" className="button">Save</button>
+                <button type="button" className="button">Check Balance</button>
+                <button type="button" className="success button">Save</button>
+              </div>
+	        	</form>
+        	</div>
+      	);
    }
 }
 export default CryptoLockApp;
--- main.js
+++ main.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CryptoLockApp from './CryptoLockApp.jsx';
 
+$(document).foundation();
+
 ReactDOM.render(<CryptoLockApp />, document.getElementById('app'));
